c9fef72cb356852d21f1ee4b3e6d36ef84e014e7
Akshiv20 <queenprince20@gmail.com>
2025-02-21 12:31:33 +0530

    Code Changes Done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello All! Let us explore continuous Delivery Pipeline'));
+app.get('/', (req, res) => res.send('Hello All! New Version Updated'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
