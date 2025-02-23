2155a54039fe7516a3794e0c1b2c24abe0a89afc
shalwin04 <shalwinsanju.25cs@licet.ac.in>
2025-02-23 12:43:18 +0530

    app.js updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello All! New Version Updated'));
+app.get('/', (req, res) => res.send('Hello All! This is version 1'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
